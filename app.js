cacaee09ed8236cce6811528464a1e19704e9f9c
Yaseen <fantasticyaseenshariff@gmail.com>
2021-12-14 23:29:45 +0530

    update port 1

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var http = require('http');
 //create a server object:
 
+var port = process.env.PORT || 3000;
+
+
 http.createServer(function (req, res) {
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
-}).listen(5000); //the server object listens on port 8080
+}).listen(port); //the server object listens on port 8080
 
 
-var port = (process.env.PORT || '3000');
-app.set('port', port);
+
+// app.set('port', port);
 
 
 // Console will print the message
